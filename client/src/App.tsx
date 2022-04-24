922562db741d03af7537780244ed668d3724debd
spcmnd <spcmndd@protonmail.com>
2022-04-24 03:46:26 -0700

    Add environment handling and call API root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
+import { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+import heyvHttp from './core/http/heyv-http';
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    heyvHttp
+      .get('/')
+      .then((res: AxiosResponse): { message: string } => res.data)
+      .then((data: { message: string }): void => setMessage(data.message));
+  }, []);
+
   return (
     <div className="App">
       <h1>Heyv</h1>
+      <p>{message}</p>
     </div>
   );
 }
