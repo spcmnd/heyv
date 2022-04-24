72ab95cbdeb94d1f876d60968fed4343a8606111
spacemind <spcmndd@protonmail.com>
2022-04-24 13:21:39 +0200

    (feat): setup React application (#2)
    * Initialize React application
    
    * Add environment handling and call API root

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+import heyvHttp from './core/http/heyv-http';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    heyvHttp
+      .get('/')
+      .then((res: AxiosResponse): { message: string } => res.data)
+      .then((data: { message: string }): void => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Heyv</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
