ab9c8de4d535f781a761044de76c90d6ede69f26
spcmnd <spcmndd@protonmail.com>
2022-04-24 21:29:21 +0200

    Use var env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,16 @@
-import 'dotenv/config';
-
 import cors from 'cors';
+import 'dotenv/config';
 import express from 'express';
 import helmet from 'helmet';
 import rootController from './api/root-controller';
 import { environment } from './config/env';
 
+const { corsOrigin, port } = environment;
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: corsOrigin }));
 app.use(helmet());
 app.use(express.json());
 app.use('/api/v1', rootController);
 
-const port = 8000;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
-  console.log(environment);
-});
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
