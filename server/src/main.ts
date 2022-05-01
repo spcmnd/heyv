f1dc9cc01b54756e9ab86c2363dc496d8582745c
spacemind <spcmndd@protonmail.com>
2022-05-01 21:37:13 +0200

    (fix): add cors and dockerfile (#7)
    * Remove old server and initialize Nest
    
    * Fix package.lock
    
    * Initialize Nest application with Task entity
    
    * Update Dockerfile & add CORS config from env
    
    * Trying to fix Docker env
    
    * Update node version in Dockerfile

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGIN'),
+  });
   app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8000);
+
+  await app.listen(parseInt(configService.get<string>('PORT')) ?? 8000);
 }
 bootstrap();
