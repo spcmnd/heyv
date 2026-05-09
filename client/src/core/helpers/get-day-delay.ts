49c592587434dc87a277840dfe99dccd78eeb2e3
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-05-09 21:15:35 +0200

    refactor(all): Nuke the existing apps

--- client/src/core/helpers/get-day-delay.ts
+++
@@ -1,5 +0,0 @@
-const getDayDelay = (date1: Date, date2: Date = new Date()): number => {
-  return Math.round((date2.getTime() - date1.getTime()) / (1000 * 3600 * 24));
-};
-
-export default getDayDelay;
