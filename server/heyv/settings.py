167a19b033d52d691138e29c5bb06c71b163d141
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-07-07 13:08:36 +0200

    config(auth): Add useful settings for simplejwt.

--- server/heyv/settings.py
+++ server/heyv/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,5 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
+
+SIMPLE_JWT = {"ROTATE_REFRESH_TOKENS": True, "REFRESH_TOKEN_LIFETIME": timedelta(days=30), "UPDATE_LAST_LOGIN": True}
