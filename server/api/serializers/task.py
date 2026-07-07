836e4b44610c8019b9fe77fd886e0361e5f52ecf
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-07-07 13:32:13 +0200

    feat(api): Display the name of the category in the API response.

--- server/api/serializers/task.py
+++ server/api/serializers/task.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from app.models.category import Category
 from app.models.task import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field="name", many=True, queryset=Category.objects.all())
+
     class Meta:
         model = Task
         fields = ("id", "name", "description", "created_at", "due_date", "last_done_date", "interval", "category")
