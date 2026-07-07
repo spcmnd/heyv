3dd705f0fa162d3da2f9805f41624f213d2076f1
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-07-07 12:40:18 +0200

    fix(api): Fix saving issues and creation due date issue

--- server/api/views/task.py
+++ server/api/views/task.py
@@ -9,16 +9,20 @@ class TaskListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
 
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        instance.recalculate_due_date()
+        instance.save(update_fields=["due_date"])
+
 
 class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
 
     def perform_update(self, serializer):
-        instance = serializer.instance
-        old_last_done_date = instance.last_done_date
+        old_last_done_date = serializer.instance.last_done_date
+        instance = serializer.save()
 
         if old_last_done_date != instance.last_done_date:
             instance.recalculate_due_date()
-
-        instance = serializer.save()
+            instance.save(update_fields=["due_date"])
