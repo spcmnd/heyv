d8775ee98f7c111b416f9e94b5bc76ba1e1a4caf
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-07-07 13:07:06 +0200

    feat(api): Add the token refresh endpoint.

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views.auth import LoginAPIView
 from .views.task import TaskListCreateAPIView, TaskRetrieveUpdateDestroyAPIView
@@ -6,6 +7,7 @@ from .views.user import UserRetrieveAPIView
 
 urlpatterns = [
     path("auth/login/", LoginAPIView.as_view(), name="api-auth-login"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="api-auth-refresh"),
     path("task/", TaskListCreateAPIView.as_view(), name="api-task-list-create"),
     path("task/<int:pk>/", TaskRetrieveUpdateDestroyAPIView.as_view(), name="api-task-retrieve-update-destroy"),
     re_path(r"^user/(?P<pk>[0-9]+|me)/$", UserRetrieveAPIView.as_view(), name="api-user-retrieve"),
