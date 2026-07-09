464f518d69f6232124c3ca8877e0e44da38fa47a
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-07-09 13:27:53 +0200

    setup(client): Add colors and utilities. Setup global SCSS mimport.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,21 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData(source: string, filePath: string) {
+          if (filePath.includes("/scss/")) return source;
+
+          return '@use "scss" as *;\n' + source;
+        },
+        loadPaths: [path.resolve("src")],
+      },
+    },
+  },
 });
