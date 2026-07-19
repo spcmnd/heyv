c692649cd4a270f3b822e6c3acec2f7ae1f5465a
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-07-19 23:16:43 +0200

    feat(client): Add antd UI library with custom theme.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
 import "./App.scss";
 
 import Sidebar from "./components/layout/Sidebar";
+import { ConfigProvider } from "antd";
 
 function App() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <main className="flex-1 p-8"></main>
-    </div>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#46566b",
+        },
+      }}
+    >
+      <div className="flex h-screen">
+        <Sidebar />
+        <main className="flex-1 p-8"></main>
+      </div>
+    </ConfigProvider>
   );
 }
 
