0e4edde7cf1105fbbcf6f66274fa28f20daba263
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-08-05 22:46:57 +0200

    setup(server): Regroup into pyproject.toml and fix some lints.

--- server/app/forms.py
+++ server/app/forms.py
@@ -1,22 +1,22 @@
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth import forms
 
 from .models import User
 
 
-class UserCreationForm(UserCreationForm):
+class UserCreationForm(forms.UserCreationForm):
     """User creation form for Django admin."""
 
-    class Meta:
+    class Meta(forms.UserCreationForm.Meta):
         """Meta class to configure the form."""
 
         model = User
         fields = ("username", "email")
 
 
-class UserChangeForm(UserChangeForm):
+class UserChangeForm(forms.UserChangeForm):
     """User update form for Django admin."""
 
-    class Meta:
+    class Meta(forms.UserChangeForm.Meta):
         """Meta class to configure the form."""
 
         model = User
