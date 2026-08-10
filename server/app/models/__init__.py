90aadefcdd4cf7dd8b7efeae33c3494277d0f9d5
spacemind <39620512+spcmnd@users.noreply.github.com>
2026-08-10 22:37:52 +0200

    lint(server): Organize imports with Ruff.

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,9 +1,8 @@
-from .user import User
-
 from .category import Category
 from .recurrence_rule import RecurrenceRule
 from .room import Room
 from .task_occurrence import TaskOccurrence
 from .task_template import TaskTemplate
+from .user import User
 
 __all__ = ["User", "TaskTemplate", "TaskOccurrence", "Room", "Category", "RecurrenceRule"]
